fbeab0db81f3c25ca29cd500770cab0975e86609
Graeme Ford <Graeme.Ford@tuks.co.za>
2023-06-15 00:02:33 +0200

    Updated configure.py best practices and python version

--- configure.py
+++ configure.py
@@ -1,39 +1,36 @@
 # %%
 from json import load
 from os.path import abspath, dirname, join
-from subprocess import run
 
-with open(join("config", "config.json"), "r") as file_content:
+with open(join("config", "config.json"), "r", encoding="UTF-8") as file_content:
     config = load(file_content)
 
 PBS_Headers = [
     "#!/usr/bin/env bash",
     "#PBS -q long",
     "#PBS -l walltime=900:00:00",
-    "#PBS -l nodes=1:ppn=1",
+    "#PBS -l nodes=1:ppn=2",
     "#PBS -N Snakemake",
 ]
 
 PBS_Body = [
-    "module load python-3.8.2",
-    "cd {};".format(dirname(abspath(__file__))),
+    "module load python-3.11.3",
+    f"cd {dirname(abspath(__file__))};",
     "snakemake --cluster-config config/cluster.json --profile config/PBS-Torque-Profile",
 ]
 
 if "environment" in config:
     if "email" in config["environment"]:
         if "condition" in config["environment"]["email"]:
-            email_conditions = "#PBS -k "
+            EMAIL_CONDITIONS = "#PBS -k "
             for condition in config["environment"]["email"]["conditions"]:
-                email_conditions += condition
-            PBS_Headers.append(email_conditions)
+                EMAIL_CONDITIONS += condition
+            PBS_Headers.append(EMAIL_CONDITIONS)
         if "address" in config["environment"]["email"]:
             PBS_Headers.append("#PBS -M " + config["environment"]["email"]["address"])
 
 
-with open("run.sh", "w") as file:
+with open("run.sh", "w", encoding="UTF-8") as file:
     file.writelines("\n".join(PBS_Headers + PBS_Body))
-
-# run(["qsub", ".run.sh"], shell=True)
 
 # %%
